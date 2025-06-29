a5728db242adafa650e649ddec1e7082ea0cfe5f
ibrahimkatada <ibrahimkatada@icloud.com>
2025-06-29 09:00:11 +0300

    Finish

--- Rundom Number/MainViewController.swift
+++ Rundom Number/MainViewController.swift
@@ -7,13 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class MainViewController: UIViewController {
+    
+    @IBOutlet var minimumValueLabel: UILabel!
+    @IBOutlet var maximumValueLabel: UILabel!
+    @IBOutlet var randomValueLabel: UILabel!
+    
+    @IBOutlet var getRandomNumberButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        getRandomNumberButton.layer.cornerRadius = 15
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let settingsVC = segue.destination as? SettingsViewController
+        
+        settingsVC?.minimumValue = minimumValueLabel.text
+        settingsVC?.maximumValue = maximumValueLabel.text
     }
 
-
+    @IBAction func getRundomNumberTapped() {
+        let minimumMumber = Int(minimumValueLabel.text ?? "") ?? 0
+        let maximumNubmer = Int(maximumValueLabel.text ?? "") ?? 100
+        
+        randomValueLabel.text = Int.random(in: minimumMumber...maximumNubmer).formatted()
+    }
+    
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        let settingsVC = segue.source as? SettingsViewController
+        minimumValueLabel.text = settingsVC?.minimumValueTF.text
+        maximumValueLabel.text = settingsVC?.maximumValueTF.text
+    }
 }
 
