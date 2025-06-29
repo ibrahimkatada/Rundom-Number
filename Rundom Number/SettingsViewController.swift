1a919c3c2d82d7568574f75c7ba5dcfcfae71fbd
ibrahimkatada <ibrahimkatada@icloud.com>
2025-06-29 09:00:53 +0300

    Finished

--- Rundom Number/SettingsViewController.swift
+++ Rundom Number/SettingsViewController.swift
@@ -7,23 +7,23 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
-
+final class SettingsViewController: UIViewController {
+    @IBOutlet var minimumValueTF: UITextField!
+    @IBOutlet var maximumValueTF: UITextField!
+    
+    var minimumValue: String!
+    var maximumValue: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        minimumValueTF.text = minimumValue
+        maximumValueTF.text = maximumValue
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func canselButtonAction() {
+        dismiss(animated: true)
     }
-    */
-
+    
 }
